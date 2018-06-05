dff3430e8067edbc0206da496273f3ce7cf5fca7
Matias Mendez Corridoni <matim97@gmail.com>
2018-06-05 16:59:56 -0300

    Arreglado SP para hacer login, hecho trigger para inhabilitar usuario luego de 3 intentos fallidos de login. Fixes #2

--- functions.sql
+++ functions.sql
@@ -1,24 +1,28 @@
 USE GD1C2018;
-
-CREATE FUNCTION MATOTA.loginUsuario(@username varchar(10), @password varchar(20))
-RETURNS BIT
+GO
+CREATE PROCEDURE MATOTA.loginUsuario(@username varchar(10), @password varchar(20)) AS
 BEGIN
 	DECLARE @pwd CHAR(64);
 	SELECT @pwd = password FROM MATOTA.Usuario WHERE username = @username;
 
 	IF(@pwd = (SELECT CONVERT(NVARCHAR(64),HashBytes('SHA2_256', @password),2)) )
-		RETURN 1;
-	ELSE
 		BEGIN
-			UPDATE MATOTA.Usuario SET intentosPassword += 1;
-			RETURN 0;
+			UPDATE MATOTA.Usuario SET intentosPassword = 0;
+			RETURN 1;
 		END
-END
 
+	UPDATE MATOTA.Usuario SET intentosPassword += 1;
+	RETURN 0;
+END
+GO
 CREATE TRIGGER MATOTA.OnUsuarioUpdate ON MATOTA.Usuario
 AFTER UPDATE AS
 BEGIN
 	IF(UPDATE(intentosPassword))
-		IF( (SELECT TOP 1 intentosPassword FROM inserted) == 3)
-			UPDATE MATOTA.Usuario SET habilitado = 0 WHERE 
+		BEGIN
+			DECLARE @intentos SMALLINT, @usuario INT;
+			SELECT TOP 1 @intentos = intentosPassword, @usuario = idUsuario FROM inserted;-- El top 1 es porque si alguien nos tira un update desde afuera explota todo (update de muchas filas).
+			IF( @intentos = 3) 
+				UPDATE MATOTA.Usuario SET habilitado = 0 WHERE idUsuario = @usuario
+		END
 END
